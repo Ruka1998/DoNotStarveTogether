f18d4b258bd0c332f8451f05c1154b0ef0525a34
Ruka1998 <ranncheng@gmail.com>
2022-12-04 19:55:56 +0800

    new

---
+++ DoNotStarveTogether/Cluster_1/Master/modoverrides.lua
@@ -0,0 +1,34 @@
+return {
+    ["workshop-347079953"]={
+      configuration_options={ DFV_Language="EN", DFV_MinimalMode="default" },
+      enabled=true 
+    },
+    ["workshop-362175979"]={ configuration_options={ ["Draw over FoW"]="disabled" }, enabled=true },
+    ["workshop-378160973"]={
+      configuration_options={
+        ENABLEPINGS=true,
+        FIREOPTIONS=2,
+        OVERRIDEMODE=false,
+        SHAREMINIMAPPROGRESS=true,
+        SHOWFIREICONS=true,
+        SHOWPLAYERICONS=true,
+        SHOWPLAYERSOPTIONS=2 
+      },
+      enabled=true 
+    },
+    ["workshop-666155465"]={
+      configuration_options={
+        chestB=-1,
+        chestG=-1,
+        chestR=-1,
+        display_hp=-1,
+        food_estimation=-1,
+        food_order=0,
+        food_style=0,
+        lang="auto",
+        show_food_units=-1,
+        show_uses=-1 
+      },
+      enabled=true 
+    } 
+  }
